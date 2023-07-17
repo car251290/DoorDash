ff702939ebbce409cc4b338a36674896c86e57b8
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2023-07-17 13:51:19 -0700

    Update get.js

--- get.js
+++ get.js
@@ -1,13 +1,16 @@
- import { DoorDashClient } from "@doordash/sdk";
- import "doten/config"
+import {DoorDashClient} from '@doordash/sdk'
+import {v4 as uuidv4} from "uuid";
+import "dotenv/config"
 
- const client = new DoorDashClient(PASTE_YOUR_ACCESS_KEY_HERE);
+const DoorDashClient = require('@doordash/sdk')
+const client = new DoorDashClient.DoorDashClient(`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImRkLXZlciI6IkRELUpXVC1WMSJ9.eyJhdWQiOiJkb29yZGFzaCIsImV4cCI6MTY4ODY3MTQ0OCwiaWF0IjoxNjg4NjY5NjQ4LCJpc3MiOiI2MDdhMTgyMC01MjMwLTRiN2YtOGZiZC0wNzZmMWI0MDJhNDgiLCJraWQiOiJkNTlhZjU1ZC1mNGMzLTRhZTgtODE1MC00ZDY4NDc3YzcyZWMifQ.oBwbBsRsVMUl62ubJtDajNyEUetH_U536635JHjUafc`)
+const res = client
+  .getDelivery('D-12345')
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
- const response = client
- .getDelivery("D-12345")
- .then((response)=> {
-    console.log(response.data);
- })
- .catch((err)=> {
-    console.log(err);
- })
+  console.log(res)
